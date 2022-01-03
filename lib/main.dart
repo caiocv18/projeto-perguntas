972a028ba1caae515b07d8816b4eb9e05549d851
caiocv18 <caioviniciuscv18@gmail.com>
2022-01-03 06:26:45 -0300

    Adicionando Stateful e deixando atributos como privado

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 
 main() => runApp(new PerguntaApp());
 
-class PerguntaApp extends StatelessWidget {
-  void responder() {
+class PerguntaApp extends StatefulWidget {
+  @override
+  State<PerguntaApp> createState() => _PerguntaAppState();
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
     print("Pergunta respondida");
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    var perguntaSelecionada = 0
-
     final perguntas = [
       "Qual é a sua cor favorita?",
       "Qual é o seu animal favorito?"
@@ -23,18 +30,18 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(perguntas[0]),
+            Text(perguntas[_perguntaSelecionada]),
             RaisedButton(
               child: (Text("Resposta 1")),
-              onPressed: responder,
+              onPressed: _responder,
             ),
             RaisedButton(
               child: Text("Resposta 2"),
-              onPressed: responder,
+              onPressed: _responder,
             ),
             RaisedButton(
               child: Text("Resposta 3"),
-              onPressed: responder,
+              onPressed: _responder,
             ),
           ],
         ),
