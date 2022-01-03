4ac0023dbcb9525f93b389bd8aa1b4d6713ab543
caiocv18 <caioviniciuscv18@gmail.com>
2022-01-03 00:56:10 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(new PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas")
+          ),
+        ),
+      )
+    );
+  }
+}
