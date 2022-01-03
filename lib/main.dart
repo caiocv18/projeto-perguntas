0fb38bf363fdab55f939869a283a40bcfff7bfd1
caiocv18 <caioviniciuscv18@gmail.com>
2022-01-03 01:33:12 -0300

    Adicionando Column e utilização de botões

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,42 @@ import 'package:flutter/material.dart';
 main() => runApp(new PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print("Pergunta respondida");
+  }
+
   @override
   Widget build(BuildContext context) {
+    
+    var perguntaSelecionada = 0
+
+    final perguntas = [
+      "Qual é a sua cor favorita?",
+      "Qual é o seu animal favorito?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Perguntas")
-          ),
+          title: Text("Perguntas"),
         ),
-      )
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: (Text("Resposta 1")),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text("Resposta 2"),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text("Resposta 3"),
+              onPressed: responder,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
